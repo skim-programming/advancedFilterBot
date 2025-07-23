c53889cb14871b48ec7eab093b919e5f0db369e0
keepcry1ng <skimprogramming@gmail.com>
2025-07-23 00:39:17 -0400

    Update main.py

--- nextcordFilter/main.py
+++ nextcordFilter/main.py
@@ -1,13 +1,12 @@
 import nextcord, regex
 from nextcord.ext import commands
 from nextcord import ui
-from ofa import filter, filteredWords, substitutes
+from ofa import filter, filteredWords, substitutes, messageThreshold
 import emoji
 
-TESTING_GUILD_ID =  # Replace with your guild ID
+TESTING_GUILD_ID =   # Replace with your guild ID
 
-bot = commands.Bot(".", intents=nextcord.Intents.all())
-
+bot = commands.Bot(intents=nextcord.Intents.all())
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
@@ -16,8 +15,8 @@ async def on_ready():
 @bot.slash_command(description="Set similarity threshhold", guild_ids=[TESTING_GUILD_ID])
 @commands.has_permissions(manage_guild=True)
 async def t(interaction:nextcord.Interaction, threshhold):
+    global messageThreshold
     try:
-        global messageThreshold
         messageThreshold = float(threshhold)
         await interaction.send("Done")
         await interaction.send("Threshhold is: " + str(messageThreshold))
@@ -34,35 +33,15 @@ async def on_message(message):
         channel = message.channel
         await channel.send((message.content.replace('test', '') if message.content != 'test' else 'test') + " is working on bro", delete_after=2) # ???
         await channel.send("aight ts working on bro", delete_after=2)
-    
-    # Filter offensive content
-    offensive_patterns = [
-        # Basic pattern with character substitutions and repetitions
-        r'n+[i1!|l]+[g6q9]+[g6q9]*[e3]*[r®]+s?',
-        
-        # More specific patterns for common variations
-        r'n[i1!|l]*g{1,3}[e3]*r+s?',
-        r'n+[i1!|l]+g{1,3}[e3]*r+s?',
-        
-        # Pattern for spaced or separated characters
-        r'n[\s\-_\.]*[i1!|l]+[\s\-_\.]*[g6q9]+[\s\-_\.]*[g6q9]*[\s\-_\.]*[e3]*[\s\-_\.]*[r®]+[\s\-_\.]*s?',
-        
-        # Pattern for zero-width characters and unicode variations
-        r'n[\u200b\u200c\u200d\ufeff]*[i1!|l]+[\u200b\u200c\u200d\ufeff]*[g6q9]+[\u200b\u200c\u200d\ufeff]*[g6q9]*[\u200b\u200c\u200d\ufeff]*[e3]*[\u200b\u200c\u200d\ufeff]*[r®]+[\u200b\u200c\u200d\ufeff]*s?',
-        
-        # Add more patterns here as needed
-    ]
-    
     message_handled = False
-    if not message_handled and filter(message.content) > messageThreshold and message.author!=bot.user: # or not message_handled and regex.search(pattern, message.content, regex.IGNORECASE) 
+    if not message_handled and filter(message.content) > messageThreshold:
         await message.delete()
         await message.channel.send(f"{message.author.mention} naughty...", delete_after=2)
-
         message_handled = True
 
-    print("Threshold is: " + str(messageThreshold))
-    print(filter(message.content) >= messageThreshold)
-    print(filter(message.content))
+    #print("Threshold is: " + str(messageThreshold))
+    #print(filter(message.content) >= messageThreshold)
+    print(message.content + ": " + str(filter(message.content)))
 
     await bot.process_commands(message)
 
@@ -80,6 +59,7 @@ async def addsub(interaction: nextcord.Interaction, sub, real_letter):
             sub = sub.emojize(sub)
         except:
             print("Not a valid emoji")
+            return
     substitutes[sub] = real_letter
     await interaction.send("Sub: " + sub + " Letter: " + real_letter, delete_after=2)
 @bot.slash_command(description="Add word", guild_ids=[TESTING_GUILD_ID])
@@ -96,6 +76,7 @@ async def removesub(interaction: nextcord.Interaction, sub):
             sub = sub.emojize(sub)
         except:
             print("Not a valid emoji")
+            return
     try:
         substitutes.pop(sub)
         await interaction.send("Removed sub: " + sub, delete_after=2)
@@ -110,41 +91,5 @@ async def removeword(interaction: nextcord.Interaction, word):
         await interaction.send("Removed sub: " + word, delete_after=2)
     except:
         await interaction.send("Error (probably no such word in filter list)", delete_after=2)
-
-@bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
-@commands.has_permissions(manage_guild=True)
-async def hello(interaction: nextcord.Interaction):
-    await interaction.send("hello goat!")
-
-@bot.slash_command(description="embed / interaction test", guild_ids=[TESTING_GUILD_ID])
-@commands.has_permissions(manage_guild=True)
-async def embed(interaction: nextcord.Interaction):
-    embed = nextcord.Embed(title="hi...", description="> **This** is a __test__ embed.", color=nextcord.Color.blue())
-    embed.set_footer(text="hohohohoho", icon_url=interaction.guild.icon.url if interaction.guild.icon else interaction.user.default_avatar.url)
-    embed.set_thumbnail(url=interaction.user.avatar.url if interaction.user.avatar else interaction.user.default_avatar.url)
-
-    class drop(ui.Select):
-        def __init__(self):
-            options = [
-                nextcord.SelectOption(label="Option 1", description="First option"),
-                nextcord.SelectOption(label="Option 2", description="Second option"),
-                nextcord.SelectOption(label="Option 3", description="Third option"),
-            ]
-            super().__init__(placeholder="choose...", min_values=1, max_values=1, options=options)
-
-        async def callback(self, interaction: nextcord.Interaction):
-            await interaction.response.send_message(f"You selected: {self.values[0]}", ephemeral=True)
-
-    class view(ui.View):
-        def __init__(self):
-            super().__init__()
-            self.add_item(drop())
-
-        @ui.button(label="Click Me!", style=nextcord.ButtonStyle.primary)
-        async def button_callback(self, button: ui.Button, interaction: nextcord.Interaction):
-            await interaction.response.send_message("push my byuttowns nghhhh~", ephemeral=True)
-
-    view = view()
-    await interaction.send(embed=embed, view=view)
 
 bot.run('')
